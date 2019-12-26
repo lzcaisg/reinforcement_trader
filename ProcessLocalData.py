ffbfb98b4a001e8ed8cc41a8a2a7886f98ceeba0
LzCai <lzcai7@gmail.com>
2019-12-26 17:49:29 +0800

    Update ProcessLocalData.py

--- ProcessLocalData.py
+++ ProcessLocalData.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
-from numba import jit
 
 DIR_FILENAME = "../index"
 NAMELIST_FILENAME = "Index-v3-2009&1990.csv"
@@ -13,35 +12,24 @@ INIT_VALUE = 1000000
 START_DATE = pd.Timestamp(2009, 1, 1)
 END_DATE = pd.Timestamp(2018, 12, 31)
 namelist_df = pd.read_csv(os.path.join(DIR_FILENAME, NAMELIST_FILENAME))
-# print([namelist_df['File Name'][0]])
 
 for etfName in namelist_df['File Name']:
-# for etfName in ['Bursatil']:
-# for etfName in [namelist_df['File Name'][0]]:
+# for etfName in ['Bursatil']: #------For Testing One ETF------
+# for etfName in [namelist_df['File Name'][0]]: #------For Testing Input------
     print(etfName)
 
     # etfName = "S&P Merval"
     fileName = etfName + " Historical Data.csv"
     dfList = []
-    # for dirName in [DIR_DATA_1990, DIR_DATA_2006]:
-    #     dfList.append(csvutils.csv2df(os.path.join(DIR_FILENAME, dirName), fileName))
 
-    # longdf = pd.concat(dfList).sort_values(by=['Date']).reset_index(drop=True)
     try:
         etfDF = csvutils.csv2df(os.path.join(DIR_FILENAME, DIR_DATA_2006), fileName).sort_values(by=['Date']).reset_index(drop=True)
-        # print (etfDF)
-
-        # dateList = list(longdf['Date'])
-        # dateList = [i for i in dateList if i >= START_DATE]
 
         dateRange = pd.date_range(START_DATE, END_DATE)
         etfDF.set_index('Date', inplace=True)
         etfDF = etfDF.reindex(dateRange, fill_value=np.nan)
         priceArray = np.array(etfDF['Price'].reset_index(drop=True))
-        # print(priceArray)
-        # print(1/priceArray)
-        # print(np.outer(priceArray, 1/priceArray)[1])
-        resultMatrix = np.outer(1/priceArray, priceArray)
+        resultMatrix = np.triu(np.outer(1/priceArray, priceArray))
         '''
         outer(a, b) = [
             [a0*b0, a0*b1, ..., a0*bn],
@@ -58,34 +46,18 @@ for etfName in namelist_df['File Name']:
             [p0/pn, p1/pn, ..., pn/pn]
         ]
         
+        triu(outer(1/p, p)) = 
+        [
+            [p0/p0, p1/p0, ..., pn/p0],
+            [    0, p1/p1, ..., pn/p1],
+            ...
+            [    0,     0, ..., pn/pn]
+        ]
+        
         Therefore, the return for buying in at time t and sell at time s=t+n days will be 
         (ps/pt) = result[t][s] = result[t][t+n]
         
         '''
-        # print(resultMatrix)
         pickle.dump(resultMatrix, open(os.path.join("output", etfName + "_returnMatrix.out"), "wb"))
     except Exception as e:
         print(e)
-'''
-    deltaList = list(range(7, 365*5))
-    resultdf = pd.DataFrame(index=dateList, columns=deltaList)
-
-    latestDate = dateList[-1]
-    counter = 0
-    for currentDay in dateList:
-        for deltaday in range(7, 365 * 5):
-            sellDate = currentDay + pd.Timedelta(days=deltaday)
-            if sellDate > latestDate:
-                break
-            if not ((currentDay in dateList) and (sellDate in dateList)):
-                continue
-
-            buyPrice = longdf['Price'].loc[currentDay]
-            sellPrice = longdf['Price'].loc[currentDay + pd.Timedelta(days=deltaday)]
-            resultdf.at[currentDay, deltaday] = sellPrice / buyPrice
-        if counter % 50 == 0:
-            print(etfName+" "+str(currentDay) + " finished.")
-        counter += 1
-    print(resultdf)
-    pickle.dump(resultdf, open(os.path.join("output", etfName+"_return.out"), "wb")) 
-'''
