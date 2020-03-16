85209ae8f0215fb4ef01d5d7ca12e00d4fee05f8
LzCai-PC <lz-cai@foxmail.com>
2020-03-16 12:04:24 +0800

    Update 50k 100k

--- from github/Stock-Trading-Environment/main.py
+++ from github/Stock-Trading-Environment/main.py
@@ -104,8 +104,8 @@ testEnv  = DummyVecEnv([lambda: RebalancingEnv(df_dict=test_df_dict, col_list=co
 
 # ============ Number of days trained =============
 REPEAT_NO = 10
-# tstep_list = [200000,500000]
-tstep_list = [100000]
+tstep_list = [200000]
+# tstep_list = [50000, 100000]
 # tstep_list = [100000, 500000]
 
 
