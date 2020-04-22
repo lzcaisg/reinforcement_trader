4afc990f9e05303522dd4c4e18d9da2224704c7e
LzCai <lzcai7@gmail.com>
2020-04-22 15:27:06 +0800

    Update Codes

--- from github/Stock-Trading-Environment/start_0418.py
+++ from github/Stock-Trading-Environment/start_0418.py
@@ -2,12 +2,6 @@ import pandas as pd
 import numpy as np
 from main_0418 import *
 
-SAVE_DIR = "./output/1001"                              # Save Directory
-DATE_PREFIX = "0418"            
-VAIRABLE_PREFIX = "monthlyFreq_haveLeak_noCrisis"           # Filename: "0418-action_frequency-summary.out"
-TSTEP_LIST = [200000]                                   # List of training steps
-DF_NAMELIST = {"high": "^BVSP_new", "mid": "^TWII_new", "low": "^IXIC_new"}
-                                                        # csv file name
 
 freq_dict = {
     "daily": 1,
@@ -18,8 +12,8 @@ freq_dict = {
 
 ENV_PARAM = {
     "trans_freq": freq_dict['monthly'],
-    "have_currency_leakage": True,
-    "crisis_detection": True,
+    "have_currency_leakage": False,
+    "crisis_detection": False,
     "MDD_window": freq_dict['monthly'], 
     "reward_wait": 10,
     "MDD_threshold": 0.2
@@ -28,46 +22,60 @@ ENV_PARAM = {
 TRAIN_TEST_DATE = {                     # outer bond of the train test date
     "trainStartDate": pd.to_datetime("2005-01-01"),
     "trainEndDate":   pd.to_datetime("2014-12-31"),
-    "testStartDate":  pd.to_datetime("2000-01-01"),
-    "testEndDate":    pd.to_datetime("2008-12-31")
+    "testStartDate":  pd.to_datetime("2015-01-01"),
+    "testEndDate":    pd.to_datetime("2019-12-31")
     }
 
 
 # --- To Train a new environment --- #
-# main(   TRAINING=True, SAVE_DIR = SAVE_DIR, DATE_PREFIX = DATE_PREFIX, 
-#         VAIRABLE_PREFIX = VAIRABLE_PREFIX, DF_NAMELIST = DF_NAMELIST, 
-#         TRAIN_TEST_DATE = TRAIN_TEST_DATE, TSTEP_LIST = TSTEP_LIST,
-#         ENV_PARAM = ENV_PARAM)
 
-
-# # --- To Test an existing environment --- #
 dir_dict = {
-    "monthly": "1011",
-    "biweekly": "1012",
-    "weekly": "1013"
+    "monthly": "2000",
+    "weekly": "2001",
+    "daily": "2002",
 }
+for freq in dir_dict:
+    SAVE_DIR = "./output"+dir_dict[freq]                              # Save Directory
+    DATE_PREFIX = "0419"            
+    VAIRABLE_PREFIX = freq+"Freq_noLeak_noCrisis"           # Filename: "0418-action_frequency-summary.out"
+    TSTEP_LIST = [200000]                                   # List of training steps
+    DF_NAMELIST = {"high": "^BVSP_new", "mid": "^TWII_new", "low": "^IXIC_new"}
+                                                            # csv file name
 
-threshold_list = [0.20, 0.15, 0.10]
+    main(   TRAINING=True, SAVE_DIR = SAVE_DIR, DATE_PREFIX = DATE_PREFIX, 
+            VAIRABLE_PREFIX = VAIRABLE_PREFIX, DF_NAMELIST = DF_NAMELIST, 
+            TRAIN_TEST_DATE = TRAIN_TEST_DATE, TSTEP_LIST = TSTEP_LIST,
+            ENV_PARAM = ENV_PARAM)
 
-for thres in threshold_list:
-    for freq in dir_dict:
-        SAVE_DIR = "./output/"+dir_dict[freq]
-        LOAD_DIR = "./output/306"
-        MODEL_FILE_PREFIX = "BRZ_TW_NASDAQ-Selected_Trans-withleakage+RSI-200000-"
-        for start in range(2000, 2016):
-            testStartDate = pd.to_datetime(str(start)+"-01-01")
-            for end in range(start+4, 2020):
-                testEndDate = pd.to_datetime(str(end)+ "-12-31")
+
+# # # --- To Test an existing environment --- #
+# dir_dict = {
+#     "monthly": "1011",
+#     "biweekly": "1012",
+#     "weekly": "1013"
+# }
+
+# threshold_list = [0.20, 0.15, 0.10]
+
+# for thres in threshold_list:
+#     for freq in dir_dict:
+#         SAVE_DIR = "./output/"+dir_dict[freq]
+#         LOAD_DIR = "./output/306"
+#         MODEL_FILE_PREFIX = "BRZ_TW_NASDAQ-Selected_Trans-withleakage+RSI-200000-"
+#         for start in range(2000, 2016):
+#             testStartDate = pd.to_datetime(str(start)+"-01-01")
+#             for end in range(start+4, 2020):
+#                 testEndDate = pd.to_datetime(str(end)+ "-12-31")
                 
-                TRAIN_TEST_DATE['testStartDate'] = testStartDate
-                TRAIN_TEST_DATE['testEndDate'] = testEndDate
-                VAIRABLE_PREFIX = "TEST_"+("%.2f"%thres)+"_"+freq+"Crisis_"+str(start)+"_"+str(end)
-                ENV_PARAM['MDD_window'] = freq_dict[freq]
-                ENV_PARAM['MDD_threshold'] = thres
-                print("@@@@@@@@@@@@@@@@@@@", thres, freq, start, end, "@@@@@@@@@@@@@@@@@@@")
-                main(   TRAINING=False, SAVE_DIR = SAVE_DIR, LOAD_DIR = LOAD_DIR, MODEL_FILE_PREFIX = MODEL_FILE_PREFIX,
-                        DATE_PREFIX = DATE_PREFIX, VAIRABLE_PREFIX = VAIRABLE_PREFIX, DF_NAMELIST = DF_NAMELIST, 
-                        TRAIN_TEST_DATE = TRAIN_TEST_DATE, TSTEP_LIST = TSTEP_LIST, ENV_PARAM=ENV_PARAM)
+#                 TRAIN_TEST_DATE['testStartDate'] = testStartDate
+#                 TRAIN_TEST_DATE['testEndDate'] = testEndDate
+#                 VAIRABLE_PREFIX = "TEST_"+("%.2f"%thres)+"_"+freq+"Crisis_"+str(start)+"_"+str(end)
+#                 ENV_PARAM['MDD_window'] = freq_dict[freq]
+#                 ENV_PARAM['MDD_threshold'] = thres
+#                 print("@@@@@@@@@@@@@@@@@@@", thres, freq, start, end, "@@@@@@@@@@@@@@@@@@@")
+#                 main(   TRAINING=False, SAVE_DIR = SAVE_DIR, LOAD_DIR = LOAD_DIR, MODEL_FILE_PREFIX = MODEL_FILE_PREFIX,
+#                         DATE_PREFIX = DATE_PREFIX, VAIRABLE_PREFIX = VAIRABLE_PREFIX, DF_NAMELIST = DF_NAMELIST, 
+#                         TRAIN_TEST_DATE = TRAIN_TEST_DATE, TSTEP_LIST = TSTEP_LIST, ENV_PARAM=ENV_PARAM)
 
 
 
